e8aac3ad39ecbdeba35af2cace4c07076290e45b
Fidelis <atosfiel@gmail.com>
2016-02-16 21:26:35 -0200

    apontando root para dashboard home

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -11,7 +11,7 @@
     {
         $locationProvider.html5Mode(true);
 
-        $urlRouterProvider.otherwise('/dashboard-project');
+        $urlRouterProvider.otherwise('/dashboard-home');
 
         /**
          * Layout Style Switcher
